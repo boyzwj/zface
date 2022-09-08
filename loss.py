330543a7ce9866b23f81cfad859a434969a414f1
ZhangWenjie <boyzwj@qq.com>
2022-09-08 16:02:40 +0800

    adative loss wight

--- loss.py
+++ loss.py
@@ -9,8 +9,6 @@ def get_losses_weights(losses):
     reference:
     https://blog.csdn.net/leiduifan6944/article/details/107486857?utm_medium=distribute.pc_relevant.none-task-blog-baidujs_baidulandingword-2&spm=1001.2101.3001.4242
     '''
-    if type(losses) != torch.Tensor:
-        losses = torch.tensor(losses)
     weights = torch.div(losses, torch.sum(losses)) * losses.shape[0]
     return weights
 
@@ -30,37 +28,39 @@ class HifiFaceLoss(LossInterface):
     def __init__(self, args):
         super().__init__(args)
         self.W_adv = 0.02
+        self.W_shape = 0.01
         self.W_id = 1
-        self.W_seg = 100
         self.W_recon = 25
         self.W_cycle = 1
         self.W_lpips = 5
-        self.W_shape = 0.5
+        self.W_seg = 100
+        
+        self.weights =  torch.tensor([self.W_adv,self.W_shape,self.W_id,self.W_recon,self.W_cycle,self.W_lpips],device="cuda:0")
         self.batch_size = args["batch_size"]
-        self.face_pool = torch.nn.AdaptiveAvgPool2d((64, 64)).to("cuda").eval()
+        # self.face_pool = torch.nn.AdaptiveAvgPool2d((64, 64)).to("cuda").eval()
 
     def get_loss_G(self, G_dict):
-        L_G = 0.0
+        # L_G = 0.0
         
         # Adversarial loss
         if self.W_adv:
             # L_adv = Loss.get_BCE_loss(G_dict["d_adv"], True)
             L_adv = sum([dual_contrastive_loss(fake,real) for fake,real in zip(G_dict["d_fake"],G_dict["d_real"])])
-            L_G += self.W_adv * L_adv
+            # L_G += self.W_adv * L_adv
             self.loss_dict["L_adv"] = round(L_adv.item(), 4)
           
         # Shape loss
         if self.W_shape:
             L_shape = Loss.get_L1_loss(G_dict["q_fuse"], G_dict["q_swapped_high"]) 
             L_shape += Loss.get_L1_loss(G_dict["q_fuse"], G_dict["q_swapped_low"]) 
-            L_G += self.W_shape * L_shape/68
+            # L_G += self.W_shape * L_shape/68
             self.loss_dict["L_shape"] = round(L_shape.item(), 4)
             
         # Id loss
         if self.W_id:
             L_id = Loss.get_id_loss(G_dict["id_source"], G_dict["id_swapped_high"])
             L_id += Loss.get_id_loss(G_dict["id_source"], G_dict["id_swapped_low"])
-            L_G += L_id * self.W_id
+            # L_G += L_id * self.W_id
             self.loss_dict["L_id"] = round(L_id.item(), 4)
 
         # Seg loss
@@ -75,23 +75,25 @@ class HifiFaceLoss(LossInterface):
         if self.W_recon:
             L_recon = Loss.get_L1_loss_with_same_person(G_dict["I_swapped_high"], G_dict["I_target"], G_dict["same_person"], self.batch_size)
             L_recon += Loss.get_L1_loss_with_same_person(G_dict["I_swapped_low"],F.interpolate(G_dict["I_target"], scale_factor=0.25, mode='bilinear'), G_dict["same_person"], self.batch_size)
-            L_G += L_recon * self.W_recon
+            # L_G += L_recon * self.W_recon
             self.loss_dict["L_recon"] = round(L_recon.item(), 4)              
 
         # Cycle loss
         if self.W_cycle:
             L_cycle = Loss.get_L3_loss(G_dict["I_cycle"], G_dict["I_source"])
-            L_G += L_cycle * self.W_cycle         
+            # L_G += L_cycle * self.W_cycle         
             self.loss_dict["L_cycle"] = round(L_cycle.item(), 4)
 
 
         # LPIPS loss
         if self.W_lpips:
             L_lpips = Loss.get_lpips_loss_with_same_person(G_dict["I_swapped_high"], G_dict["I_target"],G_dict["same_person"], self.batch_size)
-            L_G += L_lpips * self.W_lpips
+            # L_G += L_lpips * self.W_lpips
             self.loss_dict["L_lpips"] = round(L_lpips.item(), 4)
 
-
+        losses = torch.tensor([L_adv,L_shape,L_id,L_recon,L_cycle,L_lpips],device="cuda:0")
+        w = get_losses_weights(losses) * self.weights
+        L_G = L_adv * w[0] + L_shape * w[1] + L_id * w[2] + L_recon * w[3] + L_cycle *w[4] + L_lpips * w[5]
         self.loss_dict["L_G"] = round(L_G.item(), 4)
         return L_G
     
--- test.py
+++ test.py
@@ -13,8 +13,11 @@ from models.reconnet import ReconNet
 
 
 if __name__ == '__main__':
+    a = torch.tensor([1,2,3,4]) 
+    b = torch.tensor([5,0,2,2])
+    print(a * b)
 
-    F_id = torch.jit.script(ParametricFaceModel(is_train=False))
+    # F_id = torch.jit.script(ParametricFaceModel(is_train=False))
     # F_id.load_state_dict(torch.load('./weights/backbone_r100.pth'))
     # F_id.eval()
     # m = torch.jit.script(Encoder())
